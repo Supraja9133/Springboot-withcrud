cbec54668c8219993bb68ccf4e6efdc1a20ed4d0
B.Supraja <130656860+Supraja9133@users.noreply.github.com>
2023-04-20 00:13:25 +0530

    uploded

---
+++ SpringBootWithCrud/src/main/java/com/Example/SpringBootWithCrud/SpringBootWithCrudApplication.java
@@ -0,0 +1,13 @@
+package com.Example.SpringBootWithCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithCrudApplication.class, args);
+	}
+
+}
